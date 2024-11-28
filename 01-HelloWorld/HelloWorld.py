98957981bad24c6264c8675ada8f400931940a1c
opilane <jelenapenkina@gmail.com>
2024-11-28 16:58:09 +0200

    Add directory and HelloWorld.py file

--- 01-HelloWorld/HelloWorld.py
+++
@@ -1,1 +0,0 @@
-print('Hello world!','Soovib Jelena',sep='\n')
---
+++ 01_Input_Output_Errors/HelloWorld.py
@@ -0,0 +1,10 @@
+from Exercise_HelloWorld import print_hello_world
+
+print('Hello world!','Soovib Jelena',sep='\n')
+print_hello_world()
+
+print("""Tere 
+Mark""")
+
+name = input("What is your name?")
+print(f"Hi {name}!")
